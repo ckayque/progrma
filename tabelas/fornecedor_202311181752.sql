613afc190c6b95abd2ec4c63ecd87f87c3d603c9
ckayque <135187579+ckayque@users.noreply.github.com>
2024-02-29 10:41:33 -0300

    Add files via upload

---
+++ tabelas/fornecedor_202311181752.sql
@@ -0,0 +1,60 @@
+INSERT INTO db_sapatos.fornecedor (nome,celular,telefone,cpf_cnpj,inscricao_estadual,email,cep,endereco,complemento,bairro,cidade,uf,observacao,ativado) VALUES
+	 ('GILMARIA','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('TIAGO MULE','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('EDUARDO SEVERINO','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('GILMAR BOTA PNEU','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('GERINHO','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('NK3 DENIS','','3732252870','','','','35524204','RUA WOLANDA B. DE FREITAS  561','','','NOVA SERRANA','MG','',1),
+	 ('ADRIANO','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('TONINHO','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('ALEMAO SANDALIAS','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('RS','','','','','','','','','','NOVA SERRANA','MG','',1);
+INSERT INTO db_sapatos.fornecedor (nome,celular,telefone,cpf_cnpj,inscricao_estadual,email,cep,endereco,complemento,bairro,cidade,uf,observacao,ativado) VALUES
+	 ('AELTON','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('RICARDO','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('DENIS PLANALTO','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('MAYCON','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('BIDU','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('JUNINHO CATERPILLAR','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('LETICIA','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('JULIO OLK','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('ALEX LA','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('POPPER','','','','','','','','','','NOVA SERRANA','MG','',1);
+INSERT INTO db_sapatos.fornecedor (nome,celular,telefone,cpf_cnpj,inscricao_estadual,email,cep,endereco,complemento,bairro,cidade,uf,observacao,ativado) VALUES
+	 ('AGNALDO','','','','','','','','','','','','',1),
+	 ('LEYA','','','','','','','','','','','','',1),
+	 ('NATAN','','','','','','','','','','','','',1),
+	 ('RAYNNER','','','','','','','','','','','','',0),
+	 ('SPORT FIRE','','','','','','','','','','','','',1),
+	 ('ZÉ RAIMUNDO','','','','','','','','','','','','',1),
+	 ('AMAURI','','','','','','','','','','','','',1),
+	 ('CYRLEIA CATERPILLA','','','','','','','','','','','','',1),
+	 ('DA COSTA','','','','','','','','','','','','',1),
+	 ('DAYCON','','','','','','','','','','','','',1);
+INSERT INTO db_sapatos.fornecedor (nome,celular,telefone,cpf_cnpj,inscricao_estadual,email,cep,endereco,complemento,bairro,cidade,uf,observacao,ativado) VALUES
+	 ('EMERSON (SNOOP)','','','','','','','','','','','','',1),
+	 ('JULIA TÊNIS INFANTIL','','','','','','','','','','','','',1),
+	 ('MAICON','','','','','','','','','','','','',1),
+	 ('NEEC','','','','','','','','','','','','',1),
+	 ('JUNINHO ALLSTAR','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('N.E (EDER)','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('RAYNNER','','','','','','','','','','NOVA SERRANA','MG','',0),
+	 ('RAYNNER','','','','','','','','','','NOVA SERRABA','MG','',1),
+	 ('JÚLIO CÉSAR','','','','','','','','','','NOVA SERRANA','MG','',1),
+	 ('ERIVALDO','','','','','','','','','','','','',1);
+INSERT INTO db_sapatos.fornecedor (nome,celular,telefone,cpf_cnpj,inscricao_estadual,email,cep,endereco,complemento,bairro,cidade,uf,observacao,ativado) VALUES
+	 ('KENNER ERIVALDO','','','','','','','','','','CRATEÚS','CE','',1),
+	 ('DAVI CHUTEIRA','88888888888','88888888888','55043010000104','1167393209697','benson8208@uorak.com','35520016','Rua Doutor Jacinto Moreira Filho','','Centro','Nova Serrana','MG','',1),
+	 ('DANILO','88998888888','88998888888','52778898000127','2234599969649','reynier1573@uorak.com','35520016','Rua Doutor Jacinto Moreira Filho','','Centro','Nova Serrana','MG','',1),
+	 ('DANI','88999999999','88999999999','03024180000130','9068470032610','huimin1094@uorak.com','35520016','Rua Doutor Jacinto Moreira Filho','','Centro','Nova Serrana','MG','',1),
+	 ('JANICE','88988888888','88988888888','05286315000160','2503675449853','hamma9100@uorak.com','35520016','Rua Doutor Jacinto Moreira Filho','','Centro','Nova Serrana','MG','',1),
+	 ('BARONE BOOTS','88988888888','88988888888','26359800000197','7194584526060','ahlem2193@uorak.com','35520016','Rua Doutor Jacinto Moreira Filho','','Centro','Nova Serrana','MG','',1),
+	 ('MARCELO','88999999999','88999999999','06991275000110','6039817997042','guadalupe1707@uorak.com','35520016','Rua Doutor Jacinto Moreira Filho','','Centro','Nova Serrana','MG','',1),
+	 ('JULIO BOA VISTA','88988888888','88988888888','62920888000138','2588767925648','fatoumata5904@uorak.com','35520016','Rua Doutor Jacinto Moreira Filho','','Centro','Nova Serrana','MG','',1),
+	 ('MARCELINHO KIDS','88988888888','88988888888','01575386000122','6326706835291','sandy1273@uorak.com','35520016','Rua Doutor Jacinto Moreira Filho','','Centro','Nova Serrana','MG','',1),
+	 ('MARCELINHO ADULTO','88988888888','88988888888','01327438000141','0673007746840','kiana7075@uorak.com','35520016','Rua Doutor Jacinto Moreira Filho','','Centro','Nova Serrana','MG','',1);
+INSERT INTO db_sapatos.fornecedor (nome,celular,telefone,cpf_cnpj,inscricao_estadual,email,cep,endereco,complemento,bairro,cidade,uf,observacao,ativado) VALUES
+	 ('DEMAR RAINHA','88999999999','88999999999','72758190000111','9120129565508','ivor2908@uorak.com','35520012','Rua Dimas Guimarães','','Centro','Nova Serrana','MG','',1),
+	 ('CHICÃO','88999999999','88999999999','38820191000100','4242303946013','donya6840@uorak.com','35520012','Rua Dimas Guimarães','','Centro','Nova Serrana','MG','',1),
+	 ('UBRAX','88888888888','88888888888','22222222222222','','UBRAX123@GMAIL.COM','35520390','Rua dos Lambaris','','Santo Expedito','Nova Serrana','MG','',1),
+	 ('ERIVALDO CROCK','','','','','','','','','','CRATEUS','CE','',1);
---
+++ tabelas/usuarios_202311181752.sql
@@ -0,0 +1,7 @@
+INSERT INTO db_sapatos.usuarios (usuario,senha,nome,sobrenome,tipo,ativado) VALUES
+	 ('admin','admin','','',1,1),
+	 ('VAL','1234','VALDENICE','MOURA',1,1),
+	 ('ckayque','1234','Carlos','Kayque',1,1),
+	 ('ANDERSON','1234','ANDERSON','LEITE',1,1),
+	 ('HEVERTON','1234','HEVERTON','LEITE',1,1),
+	 ('HIARA','1234','GOMES','DOS SANTOS',1,1);
